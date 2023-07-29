cf610ed841c0d3465f58361d9b947aea9ccf86b2
ConnorR5 <crydel2@gmail.com>
2023-07-29 12:29:16 -0400

    Update simple_script.py
    Changed Script

--- simple_script.py
+++ simple_script.py
@@ -1,1 +1,10 @@
-print("Hello GitHub")
+name = "Connor"
+
+def hello(name):
+    print(f"Hello GitHub, and hello {name}")
+
+def goodbye(name):
+    print(f"Goodbye GitHub, and goodbye {name}")
+
+hello(name)
+goodbye(name)
